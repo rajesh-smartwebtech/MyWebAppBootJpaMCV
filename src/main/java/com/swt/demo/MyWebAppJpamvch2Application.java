efe59ed270432b497326e0387bf3b4ee2e41dad6
Rajesh Sureliya <rajesh@smart-webtech.in>
2021-09-16 16:28:53 +0530

    init

---
+++ src/main/java/com/swt/demo/MyWebAppJpamvch2Application.java
@@ -0,0 +1,13 @@
+package com.swt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAppJpamvch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppJpamvch2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/swt/demo/MyWebAppJpamvch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebAppJpamvch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
